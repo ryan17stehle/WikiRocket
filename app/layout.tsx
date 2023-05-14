e9d72d0c809e76932c72b1e602ebb1ada977213a
ryan17stehle <34487497+ryan17stehle@users.noreply.github.com>
2023-05-14 06:14:13 -0700

    initial commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
-import { Inter } from 'next/font/google'
+import Navbar from './components/Navbar'
 
+import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
@@ -14,8 +15,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className="bg-slate-800">
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
